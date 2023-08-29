4b5a50ca28f5ed3bb23810950526849f95e11f4a
iltocl <136131374+iltocl@users.noreply.github.com>
2023-08-28 19:39:33 -0600

    update for the file paths

--- 2.1 video segments/generate_1m_scenes_from_videos.py
+++ 2.1 video segments/generate_1m_scenes_from_videos.py
@@ -21,36 +21,38 @@ import os
 import subprocess
 import ffmpeg
 
-def get_video_duration(input_file):
-    probe = ffmpeg.probe(input_file)
+def get_video_duration(input_video):
+    probe = ffmpeg.probe(input_video)
     video_info = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
     duration = float(video_info['duration'])
     duration_minutes = round(duration / 60, 2)
     return duration_minutes
 
-def split_video(input_file, output_dir):
-    #print(input_file)
-    file = input_file.split('/')[-1]
-    result = subprocess.run(["ffmpeg", "-i", input_file,"-map","0", "-c", "copy", "-f", "segment", "-segment_time", "60","-reset_timestamps", "1", output_dir+"/"+file.split(".")[0]+".%02d."+file.split(".")[-1]],
+def split_video(input_video, output_dir):
+    #print(input_video)
+    file = input_video.split('/')[-1]
+    result = subprocess.run(["ffmpeg", "-i", input_video,"-map","0", "-c", "copy", "-f", "segment", "-segment_time", "60","-reset_timestamps", "1", output_dir+"/"+file.split(".")[0]+".%02d."+file.split(".")[-1]],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
 
-def split_videos_from_dir(input_dir_path, output_dir, time_limit):
-    #print(input_dir_path, output_dir, time_limit)
-    lst_contenido = os.listdir(input_dir_path)
+def split_videos_from_directory(input_dir, output_dir, max_time):
+    print("Input Dir :", input_dir)
+    print("Output Dir :", output_dir) 
+    #print(max_time)
+    lst_contents = os.listdir(input_dir)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    
-    for video in lst_contenido:
-        print(video)
+    i = 0
+    for video in lst_contents:
+        video_file = input_dir +"/"+ video 
         try:
-            duration_minutes = get_video_duration(input_dir_path+video)
+            duration_minutes = get_video_duration(video_file)
         except:
             duration_minutes = -1
-                
-        if duration_minutes>0 and duration_minutes <= time_limit:
-            print(f"Video duration: {duration_minutes} minutes")
-            split_video(sub_dir_path+video, output_dir)
+        if duration_minutes > 0 and duration_minutes <= max_time:
+            i = i + 1
+            print(str(i)+"/"+str(len(lst_contents) -1), f"Video duration: {duration_minutes} minutes")
+            split_video(video_file, output_dir)
         else:
             continue
             
@@ -61,30 +63,33 @@ The following variables correspond to:
 - output_dir: directory path were the video scenes are going to be stored in the format
             video_id.number_of_scene.extension
 """
-source_dir= "../1.2 YouTube retrieved videos/1.2.2 YT videos (downloaded)/" 
-lst_content_source_dir = os.listdir(source_dir)
+videos_source_dir= "../1.2 YouTube retrieved videos/1.2.2 YT videos (downloaded)/" 
+lst_content_source_dir = os.listdir(videos_source_dir)
 
-time_limit_minutes = 20
-output_dir="./videos-to-scenes/"
-
+output_dir="./2.1.1 video segments/"
 if not os.path.exists(output_dir):
    os.makedirs(output_dir)
 
 """
 PROCESS ONE SPECIFIC DIRECTORY
 """
-i = 0
-sub_dir_name = lst_content_source_dir[i]+"/"
-sub_dir_path= source_dir + sub_dir_name
-print(sub_dir_path)
-#lst_contenido = os.listdir(sub_dir_path)
-split_videos_from_dir(sub_dir_path, output_dir+sub_dir_name, time_limit_minutes)
+#input_dir_path = input("Input Directory path to process: ")  
+i = 2
+input_dir_path = videos_source_dir + lst_content_source_dir[i]
+
+output_dir_path = output_dir + lst_content_source_dir[i] +"-segments/"
+time_limit_minutes = 20
+
+split_videos_from_directory(input_dir_path, output_dir_path, time_limit_minutes)
 
 """
 PROCESS ALL SUB DIRECTORIES IN A SOURCE DIRECTORY
 """
-for directory in lst_content_source_dir:
-    sub_dir_path = source_dir + directory + "/"
-    #print(sub_dir_path)
-    print(output_dir + directory + "/")
-    split_videos_from_dir(sub_dir_path, output_dir + directory + "/", time_limit_minutes)
+
+for directory in lst_content_source_dir[3:]:
+    print("------------------------------------------------------------------")
+    input_dir_path = videos_source_dir + directory
+    output_dir_path = output_dir + directory +"-segments/"
+    time_limit_minutes = 20
+    
+    split_videos_from_directory(input_dir_path, output_dir_path, time_limit_minutes)
